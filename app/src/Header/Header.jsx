0ef9945fa01f93637fcb05a93f18561aa0869911
Greedent <frankkeith63@gmail.com>
2024-08-15 11:06:27 +0100

    switches between pages

--- app/src/Header/Header.jsx
+++ app/src/Header/Header.jsx
@@ -1,7 +1,17 @@
+'use client'
+
+import Link from 'next/link'
+export function Page() {
+}
+
 export function Header() {
-    return (
+    function clickHandler() {
+      <Link href="/Founders">Dashboard</Link>
+    }
+  return (
         <header>
       <h1>🔥Fireplace Palace</h1>
+      <Link href="/Founders">Dashboard</Link>
     </header>
     )
 }
