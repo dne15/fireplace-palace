93153ad376e229d12a14d5b7dbbdca56c3674859
Dionne Memie <d.memie@gmail.com>
2024-08-15 13:51:49 +0100

    added flex

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,40 +1,52 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import {useState} from 'react'
-
+import Link from "next/link";
+import { useState } from "react";
 
 export function Header() {
-  let [menuStatus, setMenuStatus] = useState(false)
-    
-  function clickHandler() {
-  setMenuStatus(!menuStatus)
-  console.log(menuStatus)
-    }
+  let [menuStatus, setMenuStatus] = useState(false);
 
-    // function menuOpen( {
-    //   {menuStatus ?  }
-    // })
+  function clickHandler() {
+    setMenuStatus(!menuStatus);
+    console.log(menuStatus);
+  }
+
+  // function menuOpen( {
+  //   {menuStatus ?  }
+  // })
 
   return (
-        <header>
+    <header>
       <h1>🔥Fireplace Palace</h1>
 
-      <button onClick={clickHandler}>{menuStatus ? <img src='../images/menu-open-button.png'></img> : <img src='../images/menu-close-button.png'></img>} </button>
-      {menuStatus && (
-      <div className="Menu">
-      <nav>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/founders">Meet the Founders</Link></li>
-        </ul>
-      </nav>
+      <button onClick={clickHandler}>
+        {menuStatus ? (
+          <img
+            src="../images/menu-close-button.png"
+            className="menuButtonImg"
+          ></img>
+        ) : (
+          <img
+            src="../images/menu-open-button.png"
+            className="menuButtonImg"
+          ></img>
+        )}
+      </button>
+      <div className="menuFlexBox">
+        {menuStatus && (
+          <div className="Menu">
+            <nav>
+              <Link href="/">Home</Link>
+              <br></br>
+              <Link href="/founders">Meet the Founders</Link>
+            </nav>
+          </div>
+        )}
       </div>
-    )}
     </header>
-    )
+  );
 }
 
 //menuStatus ? display menu : don't display menu
 
-//when menus open: changes button and div opens; has 2 links 
+//when menus open: changes button and div opens; has 2 links
