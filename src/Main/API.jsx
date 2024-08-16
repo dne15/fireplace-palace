6d7d56ad4318b3229fa7445358e5f1ed5a5c3690
ajbecks <aaron_beckles@hotmail.co.uk>
2024-08-16 10:50:00 +0100

    added country api

--- src/Main/API.jsx
+++ src/Main/API.jsx
@@ -7,25 +7,30 @@ export function Reviews() {
     let [selectedCountry, createDisplayReview] = useState(null)
     let [reviewData, setReviewData] = useState(null)
     
-    console.log(reviewData)
 
     useEffect ( () => {
         if (selectedCountry){
             fetch(`https://seal-app-336e8.ondigitalocean.app/reviews?country=${selectedCountry}`)
-            .then(response => response.json)
+            .then(response => response.json())
             .then(data => setReviewData(data))
         }
 
+    
+    
     }, [selectedCountry] )
     function displayReview(country) {
         createDisplayReview(country)
     }
 return (
     <>
-<button onClick={() => displayReview("England")}className="England">England</button>
-<button onClick={() => displayReview("Wales")} className="Wales">Wales</button>
-<button onClick={() => displayReview("Scotland")} className="Scotland">Scotland</button>
-{json.stringify(reviewData)}
+    <div className="reviewButtons"> 
+    <button onClick={() => displayReview("England")}className="England">England</button>
+    <button onClick={() => displayReview("Wales")} className="Wales">Wales</button>
+    <button onClick={() => displayReview("Scotland")} className="Scotland">Scotland</button>
+</div>
+<div className="reviewText">
+    {reviewData ? JSON.stringify(reviewData.text) : null}
+</div>
 </>
 )
 }
